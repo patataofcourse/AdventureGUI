3f5a5850bd392239312db863a1ecbb6c2c84cc1c
patataofcourse <patataofcourse@gmail.com>
2021-09-10 17:17:43 +0200

    show all buttons

--- main.py
+++ main.py
@@ -1,25 +1,37 @@
 import pyglet
+pyglet.resource.path = ['resources']
+pyglet.resource.reindex()
 
-def pyglet_color(hex, alpha=0xff):
+def hex_color(hex, alpha=0xff):
     red = (hex & 0xff0000) // 0x10000
     green = (hex & 0xff00) // 0x100
     blue = hex & 0xff
     print(red, green, blue)
     return red, green, blue, alpha
 
-window = pyglet.window.Window()
-window.height = 200
-label = pyglet.text.Label('Hello, world!',
-                          font_size=36,
-                          x=window.width // 2,
-                          y=window.height // 2,
-                          anchor_x='center',
-                          anchor_y='center',
-                          color=pyglet_color(0x00a0a0))
+#import res
+btn_input = pyglet.resource.image("button_input.png")
+btn_greyed = pyglet.resource.image("button_grey.png")
+
+window = pyglet.window.Window(width=888, height=80, caption="AdventureGUI (currently just a control thing lmao)")
+
+buttons = []
+x = 8
+for b in range(9):
+    buttons.append(pyglet.sprite.Sprite(img=btn_greyed, x=x, y=8))
+    x += 72
+
+special_buttons = []
+x += 16
+for b in range(3):
+    special_buttons.append(pyglet.sprite.Sprite(img=btn_greyed, x=x, y=8))
+    x += 72
 
 @window.event
 def on_draw():
     window.clear()
-    label.draw()
-
+    for button in buttons:
+        button.draw()
+    for button in special_buttons:
+        button.draw()
 pyglet.app.run()
