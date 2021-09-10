6956b2f728048b2ca77333b00a9ae8c30adc5a1b
patataofcourse <patataofcourse@gmail.com>
2021-09-10 17:54:11 +0200

    Pressed button assets

--- res.py
+++ res.py
@@ -6,12 +6,18 @@ btn_input = pyglet.resource.image("button_input.png")
 btn_input_p = pyglet.resource.image("button_input_p.png")
 
 btn_save = pyglet.resource.image("button_save.png")
+btn_save_p = pyglet.resource.image("button_save_p.png")
 
 btn_restore = pyglet.resource.image("button_restore.png")
+btn_restore_p = pyglet.resource.image("button_restore_p.png")
 
 btn_quit = pyglet.resource.image("button_quit.png")
+btn_quit_p = pyglet.resource.image("button_quit_p.png")
 
 pressed = {
     btn_input: btn_input_p,
-    btn_grey: btn_grey
+    btn_grey: btn_grey,
+    btn_save: btn_save_p,
+    btn_restore: btn_restore_p,
+    btn_quit: btn_quit_p
 }
