c9a04c154e01d3088fce48d4d74ab7e86ed2ff4a
patataofcourse <patataofcourse@gmail.com>
2021-09-23 22:37:51 +0200

    red button ig

--- res.py
+++ res.py
@@ -14,10 +14,14 @@ btn_restore_p = pyglet.resource.image("button_restore_p.png")
 btn_quit = pyglet.resource.image("button_quit.png")
 btn_quit_p = pyglet.resource.image("button_quit_p.png")
 
+btn_red = pyglet.resource.image("button_red.png")
+btn_red_p = pyglet.resource.image("button_red_p.png")
+
 pressed = {
     btn_input: btn_input_p,
     btn_grey: btn_grey,
     btn_save: btn_save_p,
     btn_restore: btn_restore_p,
-    btn_quit: btn_quit_p
+    btn_quit: btn_quit_p,
+    btn_red: btn_red_p
 }
