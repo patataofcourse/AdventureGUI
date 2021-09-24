9382921457a926e906a13daebd5336315d231f48
patataofcourse <patataofcourse@gmail.com>
2021-09-24 21:41:51 +0200

    color buttons

--- res.py
+++ res.py
@@ -17,11 +17,31 @@ btn_quit_p = pyglet.resource.image("button_quit_p.png")
 btn_red = pyglet.resource.image("button_red.png")
 btn_red_p = pyglet.resource.image("button_red_p.png")
 
+btn_orange = pyglet.resource.image("button_orange.png")
+btn_orange_p = pyglet.resource.image("button_orange_p.png")
+
+btn_yellow = pyglet.resource.image("button_yellow.png")
+btn_yellow_p = pyglet.resource.image("button_yellow_p.png")
+
+btn_green = pyglet.resource.image("button_green.png")
+btn_green_p = pyglet.resource.image("button_green_p.png")
+
+btn_blue = pyglet.resource.image("button_blue.png")
+btn_blue_p = pyglet.resource.image("button_blue_p.png")
+
+btn_purple = pyglet.resource.image("button_purple.png")
+btn_purple_p = pyglet.resource.image("button_purple_p.png")
+
 pressed = {
     btn_input: btn_input_p,
     btn_grey: btn_grey,
     btn_save: btn_save_p,
     btn_restore: btn_restore_p,
     btn_quit: btn_quit_p,
-    btn_red: btn_red_p
+    btn_red: btn_red_p,
+    btn_orange: btn_orange_p,
+    btn_yellow: btn_yellow_p,
+    btn_green: btn_green_p,
+    btn_blue: btn_blue_p,
+    btn_purple: btn_purple_p
 }
