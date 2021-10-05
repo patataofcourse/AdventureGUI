90ccee55bfa040728ed50ec6ce4c2adeb96a5249
patataofcourse <patataofcourse@gmail.com>
2021-10-05 21:33:10 +0200

    ScrollArea

--- window.py
+++ window.py
@@ -1,8 +1,10 @@
 import sys
 
 import pygame
-window = pygame.display.set_mode((888, 80))
+window = pygame.display.set_mode((888, 480))
 pygame.display.set_caption('AdventureGUI v0.2.1')
+
+import pygame_menu
 
 import button
 import res
@@ -17,7 +19,7 @@ buttons = button.ButtonSet(window)
 
 for n in range(9):
     n += 1
-    buttons.add_button(f"b{n}", button.Button.from_object(8+72*(n-1), 8, res.btn_grey))
+    buttons.add_button(f"b{n}", button.Button.from_object(8+72*(n-1), 408, res.btn_grey))
 
 def test_event():
     print("button 1 was clicked")
@@ -27,7 +29,11 @@ buttons["b1"].event = test_event
 extras = ("save", "restore", "quit")
 for b in extras:
     n = extras.index(b)+1
-    buttons.add_button(f"{b}", button.Button.from_object(600+72*n, 8, res.btn_grey))
+    buttons.add_button(f"{b}", button.Button.from_object(600+72*n, 408, res.btn_grey))
+
+scroll_world = pygame.Surface((856, 600))
+scroll_area = pygame_menu._scrollarea.ScrollArea(872, 384, area_color=(200,200,200), world = scroll_world, scrollbar_thick=16)
+scroll_surface = pygame.Surface((872, 384))
 
 def run():
     buttons["quit"].change_images(res.btn_quit)
@@ -36,7 +42,8 @@ def run():
     global closed
     while True:
         # events
-        for event in pygame.event.get() :
+        events = list(pygame.event.get())
+        for event in events:
             if event.type == pygame.QUIT :
                 pygame.quit()
                 closed = True
@@ -47,6 +54,7 @@ def run():
                 buttons()
             if event.type == pygame.MOUSEBUTTONUP:
                 buttons.release()
+        scroll_area.update(events)
 
         # closes loop if pygame has been quit through an event
         if closed:
@@ -55,7 +63,10 @@ def run():
         # rendering
         window.fill(misc.color("333")) # background color, to be changed
         buttons.draw()
+        scroll_area.draw(scroll_surface)
+        window.blit(scroll_surface, (8,8))
 
+        #update frame
         pygame.display.update()
         clock.tick(fps)
     pygame.quit()
