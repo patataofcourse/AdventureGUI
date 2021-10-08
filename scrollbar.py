226d48261e44f638f1782cbce084b9acf96360da
patataofcourse <patataofcourse@gmail.com>
2021-10-08 19:44:27 +0200

    Basic scrollbar class

--- scrollbar.py
+++ scrollbar.py
@@ -9,9 +9,16 @@ class ScrollBar:
         self.surface_height = self.surface.get_rect().height
 
         self.pos = 0
-        self.pos_height = self.height - (self.surface_height - show_height)
+        if self.surface_height - show_height < height - 32:
+            self.pos_height = height - (self.surface_height - show_height)
+            self.equivalence = 1
+        else:
+            self.pos_height = 32
+            self.equivalence = height - 32
+
         print(self.height, self.pos_height)
 
     def draw(self, surface, x, y):
         pygame.draw.rect(surface, (0xe0,0xe0,0xe0), pygame.Rect(x, y, self.width, self.height))
         pygame.draw.rect(surface, (0xc0, 0xc0, 0xc0), pygame.Rect(x, y+self.pos, self.width, self.pos_height))
+
